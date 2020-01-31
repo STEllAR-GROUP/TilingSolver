76a63b106b6ee37696b9999a23a7c148ab89374b
folshost <maxwellr96@gmail.com>
2020-01-31 11:16:33 -0600

    Adding visit functions

--- program.py
+++ program.py
@@ -19,19 +19,47 @@ ops = {"add": [add_one], "mul": [cannon_prod, dot_2d2d]}
 class TilingProblemBuilder:
     def __init__(self):
         self.op_count = {"add": 0, "mul": 0}
+        self.vertex_count = {}
         self.edges = []
         self.vertices = []
 
 
 class TilingParser(ast.NodeVisitor):
-    def __init__(self):
+    def __init__(self, state):
         print("hello")
+        self.state = state
 
     def generic_visit(self, node):
         ast.NodeVisitor.generic_visit(self, node)
 
     def visit_Assign(self, node):
         print("h")
+
+    def Name(self, node):
+        id, ctx = node.value.id, type(node.value.ctx).__name__
+        defined_before = False
+        new_definition = (ctx == "Store")
+        # See if this has been defined before
+        if new_definition:
+            for id_list, ctx_list in self.state.vertices:
+                if id == id_list and ctx_list == "Store":
+                    try:
+                        count = self.state.vertex_count[id]
+                        new_id = id + "_" + str(count)
+                        self.state.vertex_count[id] += 1
+                        id = new_id
+                    except KeyError:
+                        self.state.vertex_count[id] = 1
+                        id = id+'_1'
+
+
+
+        if (id, ctx) not in self.state.vertices:
+            assert(ctx != "Load")
+            self.state.vertices.append((id, ctx))
+        else:
+            if ctx == "Store":
+                self.state.vertices.append(id)
 
 
 class Program:
